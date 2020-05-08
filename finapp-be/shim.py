34221fdede23a5c6acd4a69fb08fd225582296d4
Mauro <39047808+mauroseb@users.noreply.github.com>
2020-05-08 20:16:00 +0200

    Create shim.py

---
+++ finapp-be/shim.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "HELLO WORLD"
+
+@app.route('/premarket')
+def premarket():
+    return 'Premarket outstanding variations are:'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
