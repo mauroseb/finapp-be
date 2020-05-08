cd93ed87ffbb57001fe86ca11c1776a0ad6674cc
Mauro <39047808+mauroseb@users.noreply.github.com>
2020-05-08 20:33:46 +0200

    Update shim.py

--- finapp-be/shim.py
+++ finapp-be/shim.py
@@ -10,5 +10,14 @@ def main():
 def premarket():
     return 'Premarket outstanding variations are:'
 
+
+@app.route('/price')
+def price():
+    return 'Price of symbol is X'
+
+@app.route('/intraday')
+def intraday():
+    return 'Intraday variation of symbol is Y'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
