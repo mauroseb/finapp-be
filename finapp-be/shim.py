162cef2930eaabd29132a43057d078c93cdbaf6c
Mauro <39047808+mauroseb@users.noreply.github.com>
2020-12-07 12:57:36 +0100

    Update shim.py

--- finapp-be/shim.py
+++ finapp-be/shim.py
@@ -1,26 +1,51 @@
 # -*- coding: utf-8 -*-
 
 import os
+import socket
+import time
+import json
 from flask import Flask
 app = Flask(__name__)
+
+def check_backend():
+    return 'success'
 
 @app.route("/")
 def main():
     return "<h1>Welcome stranger.</h1>"
 
-@app.route('/premarket')
+@app.route('/api/v1/premarket')
 def premarket():
     return 'Premarket outstanding variations are:'
 
 
-@app.route('/price/<ticker>')
+@app.route('/api/v1/price/<ticker>')
 def price(ticker):
     price = 5678.1234
     return f"Price of symbol {ticker} is {price}"
 
-@app.route('/intraday')
+@app.route('/api/v1/intraday')
 def intraday():
     return 'Intraday variation of symbol is Y'
+
+@app.route('/api/v1/healthcheck')
+def healthcheck():
+    """Healthcheck endpoint"""
+    
+    status = check_backend()
+    status_code = 200 if status == 'success' else 500
+    
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store'
+        
+    data = {
+        'hostname': socket.gethostname(),
+        'status': status,
+        'timestamp': time.time(),
+        'results': results,
+    }
+    
+    return Response(json.dumps(data), status=status_code, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
