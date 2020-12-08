143995dd4cdcecef6e2bb9504582606d4b204b5a
Mauro <39047808+mauroseb@users.noreply.github.com>
2020-12-08 18:24:13 +0100

    Create config.py

---
+++ finapp-be/config/config.py
@@ -0,0 +1,5 @@
+class Config(object):
+    """Config class for quick test"""
+    DEBUG = False
+    TICKER_LIST = ['AMZN', 'DIS', 'FOX', 'GE', 'GILD', 'GOOGL', 'HPE', 'HLT', 'HFC', 'INFO', 'IBM', 'JPM', 'LKQ', 'MSFT', 'MSCI', 'NDAQ', 'NFLX', 'NVDA', 'PLTR', 'RH', 'SPCE', 'YUM', 'ZTS']
+
